2f38a742e5f14b442240aa1c8d66cbf616920961
Simplicity18 <ansh.2427030307@muj.manipal.edu>
2025-09-14 18:52:33 +0530

    Update and rename lengthofvariables.java to objectiandclass.java

---
+++ objectiandclass.java
@@ -0,0 +1,14 @@
+class demo{
+    int a=10; String b= "ansh";
+    void show ()
+        {
+        System.out.println(a+" "+b);
+     }
+    }
+
+    class test{
+        public static void main(String[] arg){
+        demo r=new demo();
+        r.show();
+    }
+}
